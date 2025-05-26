5897cc750a4e3e9b1eb1d84554873f956f6c7012
Thomas Mildner <tm@artcodix.com>
2025-05-26 15:30:37 +0200

    Refactor CreateUser and EditUser components and removed Invite User & user role management changing

--- ui/litellm-dashboard/src/components/edit_user.tsx
+++ ui/litellm-dashboard/src/components/edit_user.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogPanel,
@@ -9,17 +9,17 @@ import {
   Text,
   Title,
   Subtitle,
-} from '@tremor/react';
+} from "@tremor/react";
 
 import {
-    Button as Button2,
-    Modal,
-    Form,
-    Input,
-    Select as Select2,
-    message,
-    InputNumber,
-  } from "antd";
+  Button as Button2,
+  Modal,
+  Form,
+  Input,
+  Select as Select2,
+  message,
+  InputNumber,
+} from "antd";
 
 import NumericalInput from "./shared/numerical_input";
 
@@ -31,7 +31,13 @@ interface EditUserModalProps {
   onSubmit: (data: any) => void;
 }
 
-const EditUserModal: React.FC<EditUserModalProps> = ({ visible, possibleUIRoles, onCancel, user, onSubmit }) => {
+const EditUserModal: React.FC<EditUserModalProps> = ({
+  visible,
+  possibleUIRoles,
+  onCancel,
+  user,
+  onSubmit,
+}) => {
   const [editedUser, setEditedUser] = useState(user);
   const [form] = Form.useForm();
 
@@ -51,47 +57,40 @@ const EditUserModal: React.FC<EditUserModalProps> = ({ visible, possibleUIRoles,
     onCancel();
   };
 
-
-
   if (!user) {
     return null;
   }
 
   return (
-
-    <Modal 
-    visible={visible} 
-    onCancel={handleCancel} 
-    footer={null}
-    title={"Edit User " + user.user_id}
-    width={1000}
+    <Modal
+      visible={visible}
+      onCancel={handleCancel}
+      footer={null}
+      title={"Edit User " + user.user_id}
+      width={1000}
     >
-        <Form
-          form={form}
-          onFinish={handleEditSubmit}
-          initialValues={user} // Pass initial values here
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          labelAlign="left"
-        >
-          <>
-            <Form.Item 
-            className="mt-8" 
-            label="User Email" 
+      <Form
+        form={form}
+        onFinish={handleEditSubmit}
+        initialValues={user} // Pass initial values here
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        labelAlign="left"
+      >
+        <>
+          <Form.Item
+            className="mt-8"
+            label="User Email"
             tooltip="Email of the User"
-            name="user_email">
-              <TextInput />
-            </Form.Item>
-
-            <Form.Item
-              label="user_id"
-              name="user_id"
-              hidden={true}
-            >
-              <TextInput />
-            </Form.Item>
-
-            <Form.Item
+            name="user_email"
+          >
+            <TextInput />
+          </Form.Item>
+          <Form.Item label="user_id" name="user_id" hidden={true}>
+            <TextInput />
+          </Form.Item>
+          {/* User Roles are managed by SSO Provider */}
+          {/* <Form.Item
               label="User Role"
               name="user_role"
             >
@@ -106,35 +105,28 @@ const EditUserModal: React.FC<EditUserModalProps> = ({ visible, possibleUIRoles,
                     ))}
             </Select2>
 
-            </Form.Item>
-
-            <Form.Item
-              label="Spend (EUR)"
-              name="spend"
-              tooltip="(float) - Spend of all LLM calls completed by this user"
-              help="Across all keys (including keys with team_id)."
-            >
-              <InputNumber min={0} step={0.01} />
-            </Form.Item>
-
-            <Form.Item
-              label="User Budget (EUR)"
-              name="max_budget"
-              tooltip="(float) - Maximum budget of this user"
-              help="Maximum budget of this user."
-            >
-              <NumericalInput min={0} step={0.01} />
-            </Form.Item>
-
-            <div style={{ textAlign: "right", marginTop: "10px" }}>
-                <Button2 htmlType="submit">Save</Button2>
-            </div>
-
+            </Form.Item> */}
+          <Form.Item
+            label="Spend (EUR)"
+            name="spend"
+            tooltip="(float) - Spend of all LLM calls completed by this user"
+            help="Across all keys (including keys with team_id)."
+          >
+            <InputNumber min={0} step={0.01} />
+          </Form.Item>
+          <Form.Item
+            label="User Budget (EUR)"
+            name="max_budget"
+            tooltip="(float) - Maximum budget of this user"
+            help="Maximum budget of this user."
+          >
+            <NumericalInput min={0} step={0.01} />
+          </Form.Item>
+          <div style={{ textAlign: "right", marginTop: "10px" }}>
+            <Button2 htmlType="submit">Save</Button2>
+          </div>
         </>
-
       </Form>
-
-    
     </Modal>
   );
 };
